c3212c82660df752d6c09307e66536db2c421d39
Helenagg <hgonzalezg18@gmail.com>
2022-11-30 20:32:20 +0000

    GET, POST, DELETE completed

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,8 +1,8 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from flask import Flask, request, jsonify, url_for, Blueprint, json
+from api.models import db, User, Family
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
@@ -13,6 +13,59 @@ def handle_hello():
 
     response_body = {
         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/family', methods=['GET'])
+def handle_family():
+    family = Family.query.all()
+    result = []
+    for person in family:
+        result.append(person.serialize())
+
+    response_body = {
+        "message": "All Ok",
+        "Family": result
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/family/<int:id>', methods=['GET'])
+def handle_person(id):
+    person = Family.query.filter_by(id = id).one()
+    result = person.serialize()
+
+    response_body = {
+        "message": "Person",
+        "Person": result
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/family', methods=['POST'])
+def create_person():
+    data = request.data
+    data = json.loads(data)
+    person = Family(first_name = data['first_name'], last_name = 'Jackson', age = data['age'], lucky_numbers = data['lucky_numbers'])
+    db.session.add(person)
+    db.session.commit()
+
+    response_body = {
+        "message": "Create a new person",
+        "Person": person.serialize()
+    }
+
+    return jsonify(response_body), 200
+
+@api.route('/family/<int:id>', methods=['DELETE'])
+def delete_person(id):
+    person = Family.query.get(id)
+    db.session.delete(person)
+    db.session.commit()
+
+    response_body = {
+        "message": "Delete Person",
     }
 
     return jsonify(response_body), 200
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -6,21 +6,25 @@ import "../../styles/home.css";
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
+	const view = () => {
+		var requestOptions = {
+			method: 'GET',
+			redirect: 'follow'
+		  };
+		  
+		  fetch("https://3001-4geeksacade-reactflaskh-2s43cxuip9x.ws-eu77.gitpod.io/api/family", requestOptions)
+			.then(response => response.text())
+			.then(result => console.log(result))
+			.catch(error => console.log('error', error));
+	}
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<>
+			<div className="text-center mt-5">
+				<h1>Family!!</h1>
+				<button onClick={view}>View Family</button>
+				<div></div>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
+		</>
 	);
 };
