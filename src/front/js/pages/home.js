c4fbcf7758276be69c916e13edb9ff3c95e89d95
Helenagg <hgonzalezg18@gmail.com>
2023-01-25 16:59:25 +0000

    GET created

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,10 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { element } from "prop-types";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
+	const [result, setResult] = useState([])
 
 	const view = () => {
 		var requestOptions = {
@@ -12,17 +14,24 @@ export const Home = () => {
 			redirect: 'follow'
 		  };
 		  
-		  fetch("https://3001-4geeksacade-reactflaskh-2s43cxuip9x.ws-eu77.gitpod.io/api/family", requestOptions)
-			.then(response => response.text())
-			.then(result => console.log(result))
+		  fetch(`${process.env.BACKEND_URL}/api/family`, requestOptions)
+			.then(response => response.json())
+			.then(result =>setResult(result))
 			.catch(error => console.log('error', error));
 	}
-
+	console.log(result)
 	return (
 		<>
 			<div className="text-center mt-5">
 				<h1>Family!!</h1>
 				<button onClick={view}>View Family</button>
+				{result.map((element) => {
+					return (
+						<div className="container">
+							<p>{element.first_name}</p>
+						</div>
+					)
+				})}
 				<div></div>
 			</div>
 		</>
